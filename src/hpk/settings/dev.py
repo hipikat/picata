5c115506f0ca0a3f74c2e8f7a1a8117fe541c678
Ada Wright <ada@hpk.io>
2024-11-28 01:17:48 +0000

    Remove ALLOWED_HOSTS from sub-settings

--- src/hpk/settings/dev.py
+++ src/hpk/settings/dev.py
@@ -9,9 +9,6 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-9yz$rw8%)1wm-l)j6q-r&$bu_n52sv=4q6)c5u8n10+5w+anec"  # noqa: S105
 
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 INSTALLED_APPS += [
