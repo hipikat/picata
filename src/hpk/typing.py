c065b99408731dd09e0d14d034b17d047d0ba345
Ada Wright <ada@hpk.io>
2024-12-05 04:18:00 +0000

    Add generic types for view args and a log line in debug_shell

--- src/hpk/typing.py
+++ src/hpk/typing.py
@@ -1,3 +1,11 @@
-"""Reusable types."""
+"""Reusable types for the hpk project."""
 
+from collections.abc import Mapping
+from typing import Any
+
+# Arguments for views
+ViewArg = tuple[Any, ...]
+ViewKwarg = Mapping[str, str | None]
+
+# Log arguments for structured logging
 LogArg = str | int | float | bool
--- src/hpk/views.py
+++ src/hpk/views.py
@@ -1,13 +1,18 @@
 """Top-level views for the site."""
 
-from typing import Any
+import logging
 
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import TemplateView
 
+from hpk.typing import ViewArg, ViewKwarg
+
+logger = logging.getLogger(__name__)
+
 
 def debug_shell(request: HttpRequest) -> None:
     """Just `assert False`, to force an exception and get to the Werkzeug debug console."""
+    logger.info("Raising `assert False` in the `debug_shell` view...")
     assert False  # noqa: B011, PT015, S101
 
 
@@ -16,6 +21,6 @@ class LandingPageView(TemplateView):
 
     template_name = "landing/landing_page.html"
 
-    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+    def get(self, request: HttpRequest, *args: ViewArg, **kwargs: ViewKwarg) -> HttpResponse:
         """Entry-point for the 'get' method."""
         return super().get(request, *args, **kwargs)
