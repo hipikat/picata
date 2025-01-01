768c4f1dc5ec5a8a7587e47f372aaec900f1a5d4
Ada Wright <ada@hpk.io>
2025-01-01 11:35:43 +0800

    Add wagtail_hooks to always sort posts in admin menu reverse chronological

--- src/hpk/wagtail_hooks.py
+++ src/hpk/wagtail_hooks.py
@@ -3,15 +3,23 @@
 See: https://docs.wagtail.org/en/stable/reference/hooks.html
 """
 
-from wagtail.hooks import register as register_hook
-# from wagtail.models import Page
+import logging
+
+from django.db.models import QuerySet, Value
+from django.db.models.functions import Coalesce
+from django.http import HttpRequest
+from wagtail import hooks
+from wagtail.models import Page
+
+logger = logging.getLogger(__name__)
 
 
-@register_hook("construct_explorer_page_queryset")
-def order_admin_menu_by_date(parent_page, pages, request):
-    # Check if the parent page is the 'blog' page by slug
+@hooks.register("construct_explorer_page_queryset")  # type: ignore[reportOptionalCall]
+def order_admin_menu_by_date(parent_page: Page, pages: QuerySet, request: HttpRequest) -> QuerySet:  # noqa: ARG001
+    """Order admin menus latest-at-top for pages with lots of children (like 'blog')."""
     if parent_page.slug == "blog":
-        # Reorder children by most recent first
-        return pages.order_by("-first_published_at")
-    # Return default queryset for all other cases
+        # Sort directly in order_by with Coalesce
+        return pages.order_by(
+            Coalesce("first_published_at", "latest_revision_created_at", Value("1970-01-01")).desc()
+        )
     return pages
